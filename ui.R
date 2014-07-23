00423d2d0d069d5566ba5246a5e8224c19915273
Connie Zabarovskaya <ConnieZ@users.noreply.github.com>
2014-07-23 10:52:17 -0500

    added tooltip to the plot

--- ui.R
+++ ui.R
@@ -1,26 +1,26 @@
 # ui.R
 library(ggvis)
 shinyUI(fluidPage(
-  titlePanel("UFO Reports"),
+  titlePanel("UFO Reports (ggvis demo)"),
   
   sidebarLayout(
     sidebarPanel(
       helpText("See the number of UFO reports in relation to 
                the number of military bases by state (USA)."),
-      #input controls
+      br(),
+      helpText("Hover over the data point to see the state and values."),
+      
       sliderInput("size", label = "Point Size",min = 10, max = 100, value = 20),
       selectInput("model", label = "Model type", 
                      choices = c("Linear" = "lm", "LOESS" = "loess"),
                      selected = "lm"),
-      #ggvis ui
+            
       uiOutput("ggvis_ui")
-      
-      
+          
     ),
     
     mainPanel(
-      #show the ggvis plot in the main plot
+      
       ggvisOutput('ggvis'))
-    
-  )
+    )
 ))
